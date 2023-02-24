0cdeafc1280d7770187a44c4748d49c28bfd51b9
Sergey Dolzhenko <dsa1972@mail.ru>
2023-02-24 12:22:24 +0300

    add(1) Example 005 Condition

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,8 +1,9 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
+
 if(username.ToLower() == "Маша")
   {
-    Console.WriteLine("Ура, это же МАША");
+    Console.WriteLine("Ура, это же МАША!");
   }
   else
   {
