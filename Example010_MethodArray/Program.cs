ca367938b64aa1c2f557cc0b6b7dae6e02aadcec
Sergey <dsa1972@mail.ru>
2023-02-26 15:34:17 +0300

    add three folders

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 45, 23, 8, 62, 39, 11, 33, 82,100 };
+
+int n = array.Length;
+int find = 45;
+
+int index = 0;
+while (index < n)
+{
+  if(array[index] == find)
+  {
+    Console.WriteLine(index);
+  }
+  //index = index + 1;
+  index++;
+}
